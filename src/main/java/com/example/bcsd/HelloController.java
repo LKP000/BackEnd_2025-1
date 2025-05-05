df5f18d3083edf43e7374292974faccea6fd2ab8
LKP <rud79730@gmail.com>
2025-05-05 23:19:55 +0900

    feat. /introduce, /json 작성

--- src/main/java/com/example/bcsd/HelloController.java
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -6,15 +6,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-
     @ResponseBody
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World!!!!!";
+    @GetMapping(value = {"/introduce", "/introduce/{name}"})
+    public String introduce(@RequestParam(name="name", required=false, defaultValue="이경표") String name) {
+        return "안녕하세요 제 이름은 " + name + "입니다!";
     }
 
-    @GetMapping("/hello2")
-    public String hello2() {
-        return "hello";
+    @ResponseBody
+    @GetMapping("/json")
+    public Json json() {
+        Json json = new Json(22, "이경표");
+        return json;
     }
 }
