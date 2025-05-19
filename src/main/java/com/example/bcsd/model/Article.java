ac0dcec3c7cc40ba0310775b86b9bebd844de609
LKP <rud79730@gmail.com>
2025-05-19 23:41:45 +0900

    feat: model 작성

--- src/main/java/com/example/bcsd/model/Article.java
+++ src/main/java/com/example/bcsd/model/Article.java
@@ -1,46 +1,106 @@
 package com.example.bcsd.model;
 
+import java.time.LocalDateTime;
+
 public class Article {
-	private int id;
+	private Long id;
+	private Board board;
+	private Member member;
 	private String title;
 	private String content;
+	private LocalDateTime createdDate;
+	private LocalDateTime modifiedDate;
 
-	public Article(int id, String title, String content) {
-		if (title==null || content==null) {
-			throw new RuntimeException();
-		}
+	public Article(Long id, Board board, Member member,
+				   String title, String content, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+		this.id = id;
+		this.board = board;
+		this.member = member;
+		this.title = title;
+		this.content = content;
+		this.createdDate = createdDate;
+		this.modifiedDate = modifiedDate;
+	}
+
+	public Article(Long id, Board board, Member member, String title, String content) {
+		this.id = id;
+		this.board = board;
+		this.member = member;
+		this.title = title;
+		this.content = content;
+	}
+
+	public Article(Board board, Member member, String title, String content) {
+		this.board = board;
+		this.member = member;
+		this.title = title;
+		this.content = content;
+	}
+
+	public Article(Long id, String title, String content) {
 		this.id = id;
 		this.title = title;
 		this.content = content;
 	}
 
-	public int getID() {
+	public Article(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+
+	public Long getId() {
 		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Board getBoard() {
+		return board;
+	}
+
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+
+	public Member getMember() {
+		return member;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
 	}
 
 	public String getTitle() {
 		return title;
 	}
 
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
 	public String getContent() {
 		return content;
 	}
 
-	public void setID(int id) {
-		this.id = id;
-	}
-
-	public void setTitle(String title) {
-		if (title==null || title.isEmpty()) {
-			throw new RuntimeException();
-		}
-		this.title = title;
-	}
-
 	public void setContent(String content) {
-		if (content==null || content.isEmpty()) {
-			throw new RuntimeException();
-		}
 		this.content = content;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(LocalDateTime modifiedDate) {
+		this.modifiedDate = modifiedDate;
 	}
 }
--- src/main/java/com/example/bcsd/model/Board.java
+++ src/main/java/com/example/bcsd/model/Board.java
@@ -1,4 +1,31 @@
 package com.example.bcsd.model;
 
 public class Board {
+    private Long id;
+    private String title;
+
+    public Board(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Board(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
