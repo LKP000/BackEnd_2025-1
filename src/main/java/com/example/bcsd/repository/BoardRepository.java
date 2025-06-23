02609360cfc2cbba6519f52d5edacea0425f6c65
LKP <rud79730@gmail.com>
2025-06-23 23:15:29 +0900

    feat: 연관관계, 영속성 전이, 고아 객체, Spring Data JPA 적용

--- src/main/java/com/example/bcsd/repository/BoardRepository.java
+++ src/main/java/com/example/bcsd/repository/BoardRepository.java
@@ -1,44 +1,17 @@
 package com.example.bcsd.repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.bcsd.model.Board;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class BoardRepository {
-    @PersistenceContext
-    private EntityManager em;
-
-    public Optional<Board> findById(Long id) {
-        Board board = em.find(Board.class, id);
-        return Optional.ofNullable(board);
-    }
-
-    public List<Board> findAll() {
-        String jpql = "SELECT b FROM Board b";
-        return em.createQuery(jpql, Board.class).getResultList();
-    }
-
-    @Transactional
-    public void save(Board board) {
-        if (board.getId() != null) {
-            em.merge(board);
-        } else {
-            em.persist(board);
-        }
-    }
-
-    @Transactional
-    public void deleteById(Long id) {
-        Board board = em.find(Board.class, id);
-        if (board != null) {
-            em.remove(board);
-        }
-    }
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    Optional<Board> findById(Long id);
+    List<Board> findAll();
+    Board save(Board board);
+    void deleteById(Long id);
 }
